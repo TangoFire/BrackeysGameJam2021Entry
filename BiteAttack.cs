e22a2066e37d9bee8633f4be928653f0ab0b9b1b
TangoFire <74326261+TangoFire@users.noreply.github.com>
2021-02-17 05:48:07 +0800

    Add files via upload

---
+++ BiteAttack.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class BiteAttack : MonoBehaviour
+{
+    public Transform bitePoint;
+    public float biteRange = 1f;
+    public float spitLaunchSpeed = 10f;
+    public LayerMask enemyLayers;
+    private Collider2D enemy;
+   
+
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Eat();
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            SpitAttack();
+        }
+    }
+
+    void SpitAttack()
+    {
+        Debug.Log("Spit");
+
+        if (enemy != null)
+        {
+            Debug.Log("Spitting out " + enemy);
+            enemy.transform.position = bitePoint.transform.position;
+            enemy.gameObject.SetActive(true);
+            enemy.attachedRigidbody.velocity = Vector2.right * spitLaunchSpeed;
+        }
+
+    }
+    
+    void Eat()
+    {
+        Debug.Log("Eat");
+        enemy = Physics2D.OverlapCircleAll(bitePoint.position, biteRange, enemyLayers).FirstOrDefault();
+        if(enemy != null)
+        {
+            Debug.Log("Hit " + enemy.name);
+            
+            enemy.gameObject.SetActive(false);
+           
+          
+        }
+
+       
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (bitePoint == null)
+            return;
+        Gizmos.DrawWireSphere(bitePoint.position, biteRange);
+    }
+}
---
+++ PlayerMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    
+        public float moveSpeed = 5f;
+
+        public Rigidbody2D rb;
+
+        
+        Vector2 input;
+     
+
+
+        private void Awake()
+        {
+            rb.GetComponent<Rigidbody2D>();
+        }
+        private void Update()
+        {
+            input.x = Input.GetAxisRaw("Horizontal");
+            input.y = Input.GetAxisRaw("Vertical");
+
+          
+        }
+        void FixedUpdate()
+        {
+            Vector2 direction = input.normalized;
+            Vector2 velocity = direction * moveSpeed;
+            Vector2 movement = velocity * Time.fixedDeltaTime;
+
+            rb.MovePosition((Vector2)transform.position + movement);
+
+            
+        }
+    
+}
