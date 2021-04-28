2b79260a143fda9f21e6408b6e26bdb7cc50baa8
TangoFire <74326261+TangoFire@users.noreply.github.com>
2021-04-28 13:02:02 -0700

    Add files via upload

--- FlyingEnemy.cs
+++ FlyingEnemy.cs
@@ -2,30 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class FlyingEnemy : MonoBehaviour
 {
     public Rigidbody2D enemyRB;
     public Transform rayOrigin;
     public LayerMask terrainLayer;
     private float raycastDistance = 2f;
-    private float flightSpeed = 10f;
+    private float flightSpeed = 5f;
+    private Collider2D flightPath;
 
 
-    void Awake()
+
+     void Awake()
     {
-     enemyRB = GetComponent<Rigidbody2D>();
+        enemyRB = GetComponent<Rigidbody2D>();
     }
 
-    
     void FixedUpdate()
     {
-        RaycastHit2D hit = Physics2D.Raycast(rayOrigin.transform.position, -Vector2.up ,raycastDistance);
+        RaycastHit2D hit = Physics2D.Raycast(rayOrigin.transform.position, -Vector2.up ,raycastDistance, terrainLayer);
 
         if (hit.collider != null)
         {
                 Debug.Log("Flying Enemy detects " + hit.collider.ToString());
 
-            
+            Fly();
         }
     }
+
+    void Fly()
+    {
+        enemyRB.velocity = Vector2.up * flightSpeed;
+    }
+
+
+    
+
+
 }
