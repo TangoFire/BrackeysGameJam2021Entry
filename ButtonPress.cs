3b4fad5c1b04456cf8e390628fa4148258edd619
TangoFire <74326261+TangoFire@users.noreply.github.com>
2021-05-18 11:00:35 -0700

    Add files via upload

--- ButtonPress.cs
+++ ButtonPress.cs
@@ -5,30 +5,48 @@ using UnityEngine;
 public class ButtonPress : MonoBehaviour
 {
     public GameObject buttonEffect;
-    private float originalButtonSizeY;
+    
     private float originalButtonSizeX;
-    private float buttonPressedSize = .05f;
-    private Transform objectSize;
+    private float originalButtonSizeY;
+    
+    private Transform button;
+
+
+    public Transform buttonUpPosition;
+    public Transform buttonDownPosition;
+    private float buttonTravel = .15f;
 
 
     void Awake()
     {
-       
-         originalButtonSizeX = this.objectSize.localScale.x;
-          originalButtonSizeY = this.objectSize.localScale.y;
-        
+
+        button = GetComponent<Transform>();
+        originalButtonSizeX = button.position.x; 
+        originalButtonSizeY = button.position.y;
     }
 
   
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        button.position = new Vector2(originalButtonSizeX, originalButtonSizeY);
+        
+
         Debug.Log("Button has been pressed.");
-        transform.localScale = new Vector3(originalButtonSizeX, buttonPressedSize);
+       
+    }
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        button.position = new Vector2(originalButtonSizeX, originalButtonSizeY);
+
+
+        Debug.Log("Button has been pressed.");
+
     }
 
     private void OnCollisionExit2D(Collision2D collision)
     {
+        button.position = new Vector2(originalButtonSizeX, originalButtonSizeY + buttonTravel);
         Debug.Log("Button has been released.");
-        transform.localScale = new Vector3(originalButtonSizeX, originalButtonSizeY);
+        
     }
 }
